2014b730d30c74484ce076a02a54258a30abf163
romainsacchi <r_s@me.com>
2022-10-04 08:32:48 +0000

    Black reformating

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,23 +1,28 @@
+import pytest
+
 from commute.validation import (
+    LIST_POWERTRAINS,
     LIST_SIZES,
     LIST_VEHICLES,
-    LIST_POWERTRAINS,
-    check_battery_type
+    check_battery_type,
 )
-import pytest
+
 
 def test_list_sizes():
     assert "Large" in LIST_SIZES
     assert "Sedan" not in LIST_SIZES
 
+
 def test_list_vehicles():
     assert "Car" in LIST_VEHICLES
     assert "Bicycle" not in LIST_VEHICLES
+
 
 def test_list_powertrains():
     assert "BEV" in LIST_POWERTRAINS
     assert "BEV-depot" in LIST_POWERTRAINS
     assert "ICEV-v" not in LIST_POWERTRAINS
+
 
 def test_check_battery_type():
     vehicle = "Bus"
@@ -37,4 +42,3 @@ def test_check_battery_type():
     with pytest.raises(KeyError) as wrapped_error:
         check_battery_type(vehicle, powertrain, battery_type)
     assert wrapped_error.type == KeyError
-
